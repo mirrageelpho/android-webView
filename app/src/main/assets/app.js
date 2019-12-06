23c4b990cf2696d2396428a63fbb7dc043de6af8
aeciocleber <aecio.pires@clinicadaobesidade.com.br>
2019-12-06 15:19:02 -0300

    Ajustes

--- app/src/main/assets/app.js
+++ app/src/main/assets/app.js
@@ -4,13 +4,11 @@ new fullpage("#fullpage", {
   anchors: ["page1", "page2", "page3"]
 });
 
-console.log(fullpage_api)
-
-fullpage_api.setAllowScrolling(false);
-fullpage_api.setRecordHistory(true);
+fullpage_api.setAllowScrolling(false); //Permite fazer rolagem da página Aceita um valor boleano
+fullpage_api.setRecordHistory(true); //Grava o histórico da navegação.
 
 let palavra_sorteada = document.querySelector("#palavra");
-let temaSorteado = "";
+let tema_sorteado = "";
 
 let times_futebol = ["Corinthians", "Vasco", "Palmeiras", "Bahia", "Flamengo"];
 let animais = ["gato", "Cachorro", "Galinha", "Periquito", "papagio"];
@@ -23,25 +21,25 @@ function playGame(tema) {
 
   switch (tema) {
     case "animais":
-      temaSorteado = sorteio(animais);
+      tema_sorteado = sorteio(animais);
       break;
     case "prof":
-      temaSorteado = sorteio(prof);
+      tema_sorteado = sorteio(prof);
       break;
     case "times":
-      temaSorteado = sorteio(times_futebol);
+      tema_sorteado = sorteio(times_futebol);
       break;
   }
 
-  palavra_sorteada.innerText = temaSorteado;
+  palavra_sorteada.innerText = tema_sorteado;
   fullpage_api.moveTo("page2");
-  fullpage_api.setAllowScrolling();
   fullpage_api.setAllowScrolling(false);
 
 }
 
 //Algorítmo para realizar o sorteio
 function sorteio(lista) {
+
   let randomico;
   for (let i = 0; i < lista.length; i++) {
     randomico = Math.floor(Math.random() * lista.length); //gerando número aleatório
@@ -49,21 +47,6 @@ function sorteio(lista) {
   return lista[randomico];
 }
 
-function loadCredits() {
-  fullpage_api.moveTo('page3');
+function moveToPage(page) {
+  fullpage_api.moveTo(page);
 }
-
-function loadOptions() {
-  //fullpage_api.moveTo('page1');
-}
-
-/* class App{
-
-   constructor(){
-    fullPage
-   }
-
-   
-} */
-
-//module.exports = new App();
