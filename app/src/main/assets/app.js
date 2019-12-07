326287a4d28f8ac128ecf34321928205de8d090f
Aécio Cleber <ramsolucoes@gmail.com>
2019-12-07 01:29:32 -0300

    Refactor

--- app/src/main/assets/app.js
+++ app/src/main/assets/app.js
@@ -1,49 +1,104 @@
 new fullpage("#fullpage", {
   autoScrolling: true,
   allowScrolling: false,
-  anchors: ["page1", "page2", "page3"]
+  anchors: ["Inicio", "Play", "Sobre"]
 });
 
-fullpage_api.setAllowScrolling(false); //Permite fazer rolagem da página Aceita um valor boleano
-fullpage_api.setRecordHistory(true); //Grava o histórico da navegação.
+const data = {
+    times_futebol: [
+      "Athletico Paranaense",
+      "Atlético Mineiro",
+      "Avaí",
+      "Bahia",
+      "Botafogo",
+      "Ceará",
+      "Chapecoense",
+      "Corinthians",
+      "Cruzeiro",
+      "CSA",
+      "Flamengo",
+      "Fluminense",
+      "Fortaleza",
+      "Goiás",
+      "Grêmio",
+      "Internacional",
+      "Palmeiras",
+      "Santos",
+      "São Paulo",
+      "Vasco da Gama"
+    ],
+    animais: [
+      "abelha",
+      "abutre",
+      "anta",
+      "aranha",
+      "canguru",
+      "cavalo",
+      "cisne",
+      "coelho",
+      "elefante",
+      "furão",
+      "lagartixa"
+    ],
+    profissoes: [
+      "ator",
+      "arquiteto",
+      "taxista",
+      "dentista",
+      "motorista",
+      "bombeiro",
+      "advogado",
+      "escritor",
+      "jornalista",
+      "jardineiro",
+      "piloto"
+    ]
+  };
 
-let palavra_sorteada = document.querySelector("#palavra");
+fullpage_api.setAllowScrolling(false); //Permite fazer rolagem da página Aceita um valor boleano
+//fullpage_api.setRecordHistory(true); //Grava o histórico da navegação.
+
+const palavra_sorteada = document.querySelector("#palavra");
 let tema_sorteado = "";
 
-let times_futebol = ["Corinthians", "Vasco", "Palmeiras", "Bahia", "Flamengo"];
-let animais = ["gato", "Cachorro", "Galinha", "Periquito", "papagio"];
-let prof = ["Pedreiro", "Programador", "Juiz", "cantor", "Professor", "Ator"];
+const opcoes = document.getElementsByClassName("opcoes");
+const navegacao = document.getElementsByClassName("btn");
 
+for (let i = 0; i < opcoes.length; i++) {
+  opcoes[i].addEventListener("click", function() {
+    playGame(opcoes[i].getAttribute("id"));
+  });
+}
+
+for (let i = 0; i < navegacao.length; i++) {
+  navegacao[i].addEventListener("click", function() {
+    moveToPage(navegacao[i].textContent);
+  });
+}
 
 function playGame(tema) {
-  
   fullpage_api.setAllowScrolling(true);
 
   switch (tema) {
     case "animais":
-      tema_sorteado = sorteio(animais);
+      tema_sorteado = sorteio(data.animais);
       break;
-    case "prof":
-      tema_sorteado = sorteio(prof);
+    case "profissoes":
+      tema_sorteado = sorteio(data.profissoes);
       break;
     case "times":
-      tema_sorteado = sorteio(times_futebol);
+      tema_sorteado = sorteio(data.times_futebol);
       break;
   }
 
   palavra_sorteada.innerText = tema_sorteado;
-  fullpage_api.moveTo("page2");
+  moveToPage("Play")
   fullpage_api.setAllowScrolling(false);
-
 }
 
 //Algorítmo para realizar o sorteio
 function sorteio(lista) {
-
-  let randomico;
-  for (let i = 0; i < lista.length; i++) {
-    randomico = Math.floor(Math.random() * lista.length); //gerando número aleatório
-  }
+  let randomico = Math.floor(Math.random() * lista.length); //gerando número aleatório
   return lista[randomico];
 }
 
